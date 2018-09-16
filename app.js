c53095394d5258fc622ed7fff6c16f6d68554023
yosayon <yosa.yon@gmail.com>
2018-09-16 17:04:58 -0400

    added packages: config, helmet, debug, morgan. Moved genres code into a separate routes directory called genres

--- app.js
+++ app.js
@@ -1,102 +1,19 @@
-const Joi = require('joi');
+
 const express = require('express');
 const app = express();
-
 const url = '/api/genres'
-const port = process.env.PORT || 3000
+const genres = require('./routes/genres')
+const morgan = require('morgan')
+const debug = require('debug')('app:startup')
+const config = require('config')
+const helmet = require('helmet')
 
 app.use(express.json())
+app.use(express.urlencoded({extend: true}))
+app.use(express.static('./public'))
+app.use(helmet())
+app.use('/api/genres', genres)
 
-const genres = [
-  { id: 1, genre: "Horror" },
-  { id: 2, genre: "Romance" },
-  { id: 3, genre: "Adventure" },
-  { id: 4, genre: "Drama" },
-  { id: 5, genre: "Animated" },
-  { id: 6, genre: "Action" },
-  { id: 7, genre: "Documentary" },
-  { id: 8, genre: "Western" },
-  { id: 9, genre: "Crime" },
-  { id: 10, genre: "Fiction" },
-  { id: 11, genre: "Fantasy" },
-  { id: 12, genre: "Mystery" },
-  { id: 13, genre: "Historical Fiction" },
-  { id: 14, genre: "Magical Realism" },
-  { id: 15, genre: "Satire" },
-  { id: 16, genre: "Urban" },
-  { id: 17, genre: "Speculative" }
-]
 
-//--------------------------------------> validations
-//validate that a genre name was given
-const validateGenre = genre => {
-  const schema = {
-    name: Joi.string().required()
-  }
-  return Joi.validate(genre, schema)
-}
-
-//validation function for uniqueness of genre name
-const isGenreUnique = genre => {
-  return genres.map(g => g.name === genre.name ? false : true
-}
-
-//validate that the genre exists in the 'db'
-const findGenre = id => {
-  const genre = genres.find(g => g.id === id)
-  return !genre ? false : genre
-}
-//home
-app.get('/', (req,res) => {
-  res.send('Welcome to Vidley')
-})
-
-//Implement CRUD through RESTful convention.
-
-//CREATE
-app.post(url, (req,res) => {
-  const { error } = validateGenre(req.body)
-  const isUnique = isGenreUnique(req.body)
-  if(error) return res.status(400).send(error.details[0].message)
-  if(!isUnique) return res.status(404).send('Genre already exists')
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name
-  }
-  genres.push(genre)
-  res.send(genre)
-})
-
-//READ
-app.get(url, (req,res) => {
-  res.send(genres)
-})
-
-app.get(`${url}/:id`, (req,res) => {
-  const genre = findGenre(parseInt(req.params.id)))
-  if(!genre) return res.status(404).send('Genre not found')
-  res.send(genre);
-})
-
-//Update
-app.put(`${url}/:id`, (req,res) => {
-  const genre = findGenre(parseInt(req.params.id)))
-  const isUnique = isGenreUnique(req.body)
-  if(!genre) return res.status(404).send('Genre not found')
-  const { error } = validateGenre(req.body)
-  if(error) return res.status(400).send(error.details[0].message)
-  if(!isUnique) return res.status(400).send('Genre already exists')
-  genre.name = req.body.name
-  res.send(genre)
-})
-
-//Delete
-app.delete(`${url}/:id`, (req,res) => {
-  const genre = findGenre(parseInt(req.params.id)))
-  if(!genre) return res.status(404).send('Genre not found')
-  const index = genres.indexOf(genre)
-  genres.splice(index, 1)
-  res.send(genre)
-})
-
-app.listen(port, () => console.log(`Listening on Port ${port}`))
+const port = process.env.PORT || 3000
+app.listen(port, () => debug(`Listening on Port ${port}`))
