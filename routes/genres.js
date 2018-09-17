6c403f24ef522390463d4a6c7f241139dd130805
yosayon <yosa.yon@gmail.com>
2018-09-17 19:50:26 -0400

    added genreSchema, connected to mongoose, added validation, removed isUnique function, added async and await to db calls,

--- routes/genres.js
+++ routes/genres.js
@@ -1,93 +1,69 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
 const Joi = require('joi');
 
-const genres = [
-  { id: 1, genre: "Horror" },
-  { id: 2, genre: "Romance" },
-  { id: 3, genre: "Adventure" },
-  { id: 4, genre: "Drama" },
-  { id: 5, genre: "Animated" },
-  { id: 6, genre: "Action" },
-  { id: 7, genre: "Documentary" },
-  { id: 8, genre: "Western" },
-  { id: 9, genre: "Crime" },
-  { id: 10, genre: "Fiction" },
-  { id: 11, genre: "Fantasy" },
-  { id: 12, genre: "Mystery" },
-  { id: 13, genre: "Historical Fiction" },
-  { id: 14, genre: "Magical Realism" },
-  { id: 15, genre: "Satire" },
-  { id: 16, genre: "Urban" },
-  { id: 17, genre: "Speculative" }
-]
-
-//--------------------------------------> validations
-//validate that a genre name was given
-const validateGenre = genre => {
-  const schema = {
-    name: Joi.string().required()
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 50,
+    lowercase: true,
   }
-  return Joi.validate(genre, schema)
-}
+})
 
-//validation function for uniqueness of genre name
-const isGenreUnique = genre => {
-  return genres.map(g => g.name === genre.name)
-}
-
-//validate that the genre exists in the 'db'
-const findGenre = id => {
-  const genre = genres.find(g => g.id === id)
-  return !genre ? false : genre
-}
-
-//Implement CRUD through RESTful convention.
+const Genre = mongoose.model('Genre', genreSchema)
 
 //CREATE
-router.post('/', (req,res) => {
+router.post('/', async (req,res) => {
   const { error } = validateGenre(req.body)
-  const isUnique = isGenreUnique(req.body)
   if(error) return res.status(400).send(error.details[0].message)
-  if(!isUnique) return res.status(404).send('Genre already exists')
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name
+  let genre = new Genre({ name: req.body.name })
+  try {
+    genre = await genre.save()
+    res.send(genre)
   }
-  genres.push(genre)
-  res.send(genre)
+  catch(ex){
+    for(field in ex.errors)
+      console.log(ex.errors[field].message)
+  }
 })
 
 //READ
 router.get('/', (req,res) => {
-  res.send(genres)
+  res.send(await Genre.find().sort('name'))
 })
 
-router.get('/:id', (req,res) => {
-  const genre = findGenre(parseInt(req.params.id))
+router.get('/:id', async (req,res) => {
+  const genre = await Genre.findById(req.params.id)
   if(!genre) return res.status(404).send('Genre not found')
   res.send(genre);
 })
 
 //Update
-router.put('/:id', (req,res) => {
-  const genre = findGenre(parseInt(req.params.id))
-  const isUnique = isGenreUnique(req.body)
-  if(!genre) return res.status(404).send('Genre not found')
+router.put('/:id', async (req,res) => {
   const { error } = validateGenre(req.body)
   if(error) return res.status(400).send(error.details[0].message)
-  if(!isUnique) return res.status(400).send('Genre already exists')
-  genre.name = req.body.name
+  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+    new: true
+  })
+  if(!genre) return res.status(404).send('Genre not found')
   res.send(genre)
 })
 
 //Delete
-router.delete('/:id', (req,res) => {
-  const genre = findGenre(parseInt(req.params.id))
+router.delete('/:id', async (req,res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id)
   if(!genre) return res.status(404).send('Genre not found')
-  const index = genres.indexOf(genre)
-  genres.splice(index, 1)
   res.send(genre)
 })
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(genre, schema);
+}
 
 module.exports = router
