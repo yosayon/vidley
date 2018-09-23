1261cfb9b5f5cc71b537c380235117b8533eb8f7
yosayon <yosa.yon@gmail.com>
2018-09-23 13:33:36 -0400

    added genre.js to models dir for separation of concerns. Moved model creation and validation into this file

--- routes/genres.js
+++ routes/genres.js
@@ -1,24 +1,21 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-const Joi = require('joi');
-
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  }
-}));
+const { Genre, validate } = require('../models/genre')
 
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
   res.send(genres);
 });
 
+router.get('/:id', async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+  res.send(genre);
+});
+
 router.post('/', async (req, res) => {
-  const { error } = validateGenre(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   let genre = new Genre({ name: req.body.name });
   genre = await genre.save();
@@ -26,7 +23,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validateGenre(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
     new: true
@@ -40,22 +37,5 @@ router.delete('/:id', async (req, res) => {
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
   res.send(genre);
 });
-
-router.get('/:id', async (req, res) => {
-  const genre = await Genre.findById(req.params.id);
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  res.send(genre);
-});
-
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-  return Joi.validate(genre, schema);
-}
-
-async function createGenre(){
-
-}
 
 module.exports = router
