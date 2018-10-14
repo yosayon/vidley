6ab5a67c2d0a7acff3c1cd4e9a5ce61f52f4294d
yosayon <yosa.yon@gmail.com>
2018-10-14 17:17:48 -0400

    eddided file name for movies to be plural, required movies

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const express = require('express');
-const app = express();
-const home = require('./routes/home');
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const home = require('./routes/home')
+const genres = require('./routes/genres')
 const customers = require('./routes/customers')
+const movies = require('./routes/movies')
 
 mongoose.connect('mongodb://localhost/vidley', {useNewUrlParser: true})
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
+  .catch(err => console.error('Could not connect to MongoDB...'))
 
-app.use(express.json());
+app.use(express.json())
 app.use('/', home);
-app.use('/api/genres', genres);
+app.use('/api/genres', genres)
 app.use('/api/customers', customers)
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`))
